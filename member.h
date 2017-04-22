c857b7d728e1a19745924b6352e8f8a99b654329
Shyam Deshmukh <shyamdeshmukh1@gmail.com>
2017-04-23 03:19:32 +0530

    Added the main & header file for library member.cpp

---
+++ member.h
@@ -0,0 +1,16 @@
+class MEMBERS
+{
+	private:
+                int member_id = 1000, year_of_joining, active = 1;
+                char member_name[32], member_type[32], phone[15], address[100];
+
+
+        public:
+
+                void delete_member(int);
+                void update_member(int, char *, char *, char *, char *, int , int);
+                void add_member(char *, char *, char *, char *, int, int);
+                void list_members();
+
+};
+
