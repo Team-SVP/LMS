2cfab348db7c16c2eb012252cfee2fbc29d264c2
Vitthal <vitthalpadwal89@gmail.com>
2017-04-25 20:55:49 +0530

    Added wrapper file for book

---
+++ include/wrapper_book.h
@@ -0,0 +1,12 @@
+#include "books.h"
+
+void wrapper_add_book(BOOKS);
+
+void wrapper_delete_book(BOOKS, int);
+
+void wrapper_update_book(BOOKS);
+
+void wrapper_book_name(BOOKS);
+
+
+void wrapper_list_book(BOOKS);
